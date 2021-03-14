b47dd87fcbcbf21c99c7074c8a4dfef951f31c27
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-14 17:03:41 +0300

    Created the holberton header file

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* HOLBERTON_H */
