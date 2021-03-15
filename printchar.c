bb8f91be4b19923ba53365f89a7366262f0e1c2a
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-16 00:34:09 +0300

    A helper function that output characters.
    This function will output a character is the format specifier 'c' is used.

---
+++ printchar.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * printchar - outputs characters
+ *
+ * @list_args - list of arguments
+ *
+ * Return: int
+ */
+int printchar(var_list list_args)
+{
+  _putchar(va_arg(list_args, int));
+  return (1);
+}
+ 
