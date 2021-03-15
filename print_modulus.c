b2d987d0dc153338d979689f88ab31fad755e421
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-16 02:55:10 +0300

    Create print_modulus.c

---
+++ print_modulus.c
@@ -0,0 +1,12 @@
+/**
+ * print_percent - prints a percent (%) symbol
+ *
+ * @list_args: list of arguments
+ *
+ * Return: number of printed characters
+ */
+int print_percent(__attribute__((unused))va_list list_args)
+{
+	_putchar('%');
+	return (1);
+}
