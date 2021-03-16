5d0b02a657736e4bc4de10cbfece6af8d59d0ab3
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-16 10:42:19 +0300

    Updated holberton.h

--- holberton.h
+++ holberton.h
@@ -5,7 +5,25 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+/**
+ * struct convert - a type for functions and symbols
+ * 
+ * @sym: The operator
+ *
+ * @f: Associated function
+ */
+struct convert
+{
+  char *sym;
+  int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_int(va_list);
+int print_modulus(va_list);
+int print_string(va_list);
+int printchar(va_list);
 
 #endif /* HOLBERTON_H */
