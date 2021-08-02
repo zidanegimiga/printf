93e6faac6050bec83ced178c723891b8968cbb5f
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-08-03 00:33:58 +0300

    encodes a string using rot13 encryption

--- print_rot13.c
+++ print_rot13.c
@@ -1,33 +1,59 @@
 #include "holberton.h"
 
 /**
- * print_rot13 - outputs the rrot13 version of a string
- * @list: string to convert
- * Return: converted string
+ * print_rot13 - encodes a string using rot13 encryption
+ * @s: string to be encoded
+ *
+ * Return: number of chars printed
  */
-int rot13(va_list list)
+int print_rot13(char *s)
 {
-int i;
-int x;
-char *str;
-char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char nor[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		      'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		      'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		      'Z', ' '};
+	char rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		      'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		      'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		      'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		      'M', ' '};
+	int count, len, count2 = 0, chars_printed = 0;
+	char now, *rot13d;
 
-str = va_arg(list, char *);
-if (str == NULL)
-return (-1);
-for (i = 0; str[i] != '\0'; i++)
-{
-for (x = 0; x <= 52; x++)
-{
-if (str[i] == s[x])
-{
-_putchar(u[x]);
-break;
-}
-}
-if (x == 53)
-_putchar(str[i]);
-}
-return (i);
+	len = 0;
+	while (*(s + len))
+		len++;
+	rot13d = malloc(sizeof(char) * len);
+	if (rot13d == NULL)
+	{
+		free(rot13d);
+		return (0);
+	}
+	len = 0;
+	while (*(s + len))
+	{
+		now = *(s + len);
+		for (count = 0; count < 53; count++)
+		{
+			if (now == nor[count])
+			{
+				*(rot13d + len) = rot[count];
+				count2 = count;
+				break;
+			}
+		}
+		if (*(rot13d + len) != rot[count2])
+			*(rot13d + len) = now;
+		if (*(rot13d + len) == '\\')
+		{
+			*(rot13d + len + 1) = *(s + len + 1);
+			len++;
+		}
+		len++;
+	}
+	*(rot13d + len) = '\0';
+	chars_printed = print_string(rot13d);
+	free(rot13d);
+	return (chars_printed);
 }
